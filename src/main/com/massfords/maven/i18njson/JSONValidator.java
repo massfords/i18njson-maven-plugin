77adfbf02f3492bd6f38ca09a7a20e33962d1787
sl <slazarus@ipswitch.com>
2015-08-10 14:52:34 -0400

    Add simple JSONValidator

---
+++ src/main/com/massfords/maven/i18njson/JSONValidator.java
@@ -0,0 +1,63 @@
+package com.massfords.maven.i18njson;
+
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author slazarus
+ */
+public class JSONValidator {
+
+    private JsonParser parser = null;
+
+    private List<String> keys = new ArrayList<String>();
+
+    public JSONValidator(File file) throws FileNotFoundException {
+        this(new FileInputStream(file));
+    }
+
+    public JSONValidator(InputStream in) {
+        parser = Json.createParser(in);
+    }
+
+    public void validate() throws I18NJsonValidationException {
+        if (!parser.hasNext()) {
+            throw new I18NJsonValidationException("File requested for JSON validation was empty");
+        }
+        JsonParser.Event event = parser.next();
+        if (event != JsonParser.Event.START_OBJECT) {
+            throw new I18NJsonValidationException("JSON contained no configuration object");
+        }
+        boolean foundEndObject = false;
+        while (parser.hasNext()) {
+            event = parser.next();
+            if (event == JsonParser.Event.END_OBJECT) {
+                foundEndObject = true;
+                break;
+            }
+            if (event != JsonParser.Event.KEY_NAME) {
+                throw new I18NJsonValidationException("Missing or invalid key in properties object");
+            }
+            final String key = parser.getString();
+            if (keys.contains(key)) {
+                throw new I18NJsonValidationException("Duplicate key \"" + key + "\" found.");
+            }
+            keys.add(key);
+            event = parser.next();
+            if (event != JsonParser.Event.VALUE_STRING) {
+                throw new I18NJsonValidationException("Found non-string value for key \"" + key + "\"");
+            }
+        }
+        if (!foundEndObject) {
+            throw new I18NJsonValidationException("Failed to find end object token!");
+        }
+    }
+
+}
